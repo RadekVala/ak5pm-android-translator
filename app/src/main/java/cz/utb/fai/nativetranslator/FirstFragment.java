7f3b3b9d25459ccc3f77e3bd7de65fee6bbc92f4
Radek Vala <radek.www@gmail.com>
2022-12-03 17:43:01 +0100

    Trigger API call in FirstFragment.java

--- app/src/main/java/cz/utb/fai/nativetranslator/FirstFragment.java
+++ app/src/main/java/cz/utb/fai/nativetranslator/FirstFragment.java
@@ -1,6 +1,7 @@
 package cz.utb.fai.nativetranslator;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,11 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.squareup.moshi.Moshi;
 
 import cz.utb.fai.nativetranslator.databinding.FragmentFirstBinding;
+import cz.utb.fai.nativetranslator.pojo.ApiResponse;
+import cz.utb.fai.nativetranslator.pojo.ResponseData;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.moshi.MoshiConverterFactory;
 
@@ -44,12 +50,16 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        binding.progressBar.setVisibility(View.GONE);
+
         binding.btnTranslate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 /*NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);*/
-                binding.txtOutput.setText(binding.txtInput.getText());
+                //binding.txtOutput.setText(binding.txtInput.getText());
+                binding.progressBar.setVisibility(View.VISIBLE);
+                getTranslation();
             }
         });
     }
@@ -59,5 +69,30 @@ public class FirstFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    private void getTranslation(){
+        String q = binding.txtInput.getText().toString();
+        String langpair = "en|cs";
+
+        Call<ApiResponse> call = apiService.getResponse(q, langpair);
+        call.enqueue(new Callback<ApiResponse>() {
+            @Override
+            public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
+                int statusCode = response.code();
+                ApiResponse data = response.body();
+                //ResponseData responseData = data.responseData;
+
+                Log.v("MYAPP", data.responseData.translatedText);
+                binding.txtOutput.setText(data.responseData.translatedText);
+                binding.progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onFailure(Call<ApiResponse> call, Throwable t) {
+                // Log error here since request failed
+            }
+        });
+    }
+
 
 }
