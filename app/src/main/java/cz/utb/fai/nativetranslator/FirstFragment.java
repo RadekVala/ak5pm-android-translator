51000954554b4d046c2eb71514ae9ff63821adab
Radek Vala <radek.www@gmail.com>
2022-12-03 17:20:50 +0100

    init of Moshi converter and Retrofit

--- app/src/main/java/cz/utb/fai/nativetranslator/FirstFragment.java
+++ app/src/main/java/cz/utb/fai/nativetranslator/FirstFragment.java
@@ -9,17 +9,31 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.squareup.moshi.Moshi;
+
 import cz.utb.fai.nativetranslator.databinding.FragmentFirstBinding;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class FirstFragment extends Fragment {
 
+    public static final String BASE_URL = "https://api.mymemory.translated.net/";
     private FragmentFirstBinding binding;
+    private MyApiEndpointInterface apiService;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        Moshi moshi = new Moshi.Builder().build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build();
+
+        apiService = retrofit.create(MyApiEndpointInterface.class);
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
 
